56a42f1e15c7dd4bebdb9932561c0bae8e7de3b6
nickskyline <duquenicolas391@gmail.com>
2023-11-27 00:30:30 -0500

    Add @NoArgsConstructor and @Data annotation for ProfileRequestDto, UserRequestDto and UserUpdateRequestDto. Remove @Getter annotation.

--- usersInnovationEducation/src/test/java/com/betek/usersInnovationEducation/domain/usecase/UserUseCaseTest.java
+++ usersInnovationEducation/src/test/java/com/betek/usersInnovationEducation/domain/usecase/UserUseCaseTest.java
@@ -1,11 +1,16 @@
 package com.betek.usersInnovationEducation.domain.usecase;
 
+import com.betek.usersInnovationEducation.adapters.driving.http.dto.request.ProfileRequestDto;
+import com.betek.usersInnovationEducation.adapters.driving.http.dto.request.UserRequestDto;
+import com.betek.usersInnovationEducation.adapters.driving.http.dto.request.UserUpdateRequestDto;
 import com.betek.usersInnovationEducation.domain.api.IAuthenticationUserInfoServicePort;
 import com.betek.usersInnovationEducation.domain.model.Profile;
 import com.betek.usersInnovationEducation.domain.model.User;
 import com.betek.usersInnovationEducation.domain.spi.IUserPersistencePort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -56,9 +61,51 @@ class UserUseCaseTest {
     }
 
     @Test
-    void updateAllInformation() {
-    }
+    public void testUpdateAllInformation() {
+        // Crear un objeto UserUpdateRequestDto con datos de prueba
+        UserUpdateRequestDto userUpdateRequestDto = new UserUpdateRequestDto();
+        UserRequestDto userRequestDto = new UserRequestDto();
+        ProfileRequestDto profileRequestDto = new ProfileRequestDto();
+        userRequestDto.setMember_name("John Doe");
+        userRequestDto.setEmail("john@example.com");
+        userRequestDto.setPassword("newpassword");
+        userRequestDto.setPhone("123456789");
+        userRequestDto.setIdCountry(1L);
+        profileRequestDto.setDescription("Updated description");
+        profileRequestDto.setProfile_image("image_url");
+        profileRequestDto.setLink_website("website_url");
+        userUpdateRequestDto.setUserRequestDto(userRequestDto);
+        userUpdateRequestDto.setProfileRequestDto(profileRequestDto);
 
+        // Simular la obtención del ID de usuario desde el token
+        when(authenticationUserInfoServicePort.getIdentifierUserFromToken()).thenReturn(123L);
+
+        // Simular la búsqueda de usuario y perfil en userPersistencePort
+        User mockedUser = new User();
+        Profile mockedProfile = new Profile();
+        when(userPersistencePort.findByIdUser(123L)).thenReturn(mockedUser);
+        when(userPersistencePort.findByIdProfile(123L)).thenReturn(mockedProfile);
+
+        // Ejecutar el método updateAllInformation
+        userUseCase.updateAllInformation(userUpdateRequestDto);
+
+        // Verificar que se actualizan correctamente los valores en el usuario y el perfil
+        assertEquals("John Doe", mockedUser.getMember_name());
+        assertEquals("john@example.com", mockedUser.getEmail());
+        assertEquals("newpassword", mockedUser.getPassword());
+        assertFalse(mockedUser.getIs_admin());
+        assertTrue(mockedUser.getState());
+        assertEquals(LocalDate.now(), mockedUser.getUpdated_at());
+        assertEquals("123456789", mockedUser.getPhone());
+        assertEquals(1L, mockedUser.getIdCountry());
+
+        assertEquals("Updated description", mockedProfile.getDescription());
+        assertEquals("image_url", mockedProfile.getProfile_image());
+        assertEquals("website_url", mockedProfile.getLink_website());
+
+        // Verificar que se llama al método updateAllInformation en userPersistencePort
+        verify(userPersistencePort).updateAllInformation(mockedUser, mockedProfile, 123L);
+    }
     @Test
     void getAnUserInformation() {
     }
