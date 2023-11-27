92b992a054004ed6d31bdf713719b1e3a100558a
nickskyline <duquenicolas391@gmail.com>
2023-11-27 00:40:28 -0500

    Add @NoArgsConstructor and @DaTa annotation for ProfileRequestDto, UserRequestDto and UserUpdateRequestDto. Remove @getter annotation.

--- usersInnovationEducation/src/main/java/com/betek/usersInnovationEducation/adapters/driving/http/dto/request/ProfileRequestDto.java
+++ usersInnovationEducation/src/main/java/com/betek/usersInnovationEducation/adapters/driving/http/dto/request/ProfileRequestDto.java
@@ -2,10 +2,13 @@ package com.betek.usersInnovationEducation.adapters.driving.http.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@Getter
+@Data
+@NoArgsConstructor
 public class ProfileRequestDto {
     @NotBlank
     private String description;
